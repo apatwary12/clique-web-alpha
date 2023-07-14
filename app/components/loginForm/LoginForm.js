1b7768a176cfed285483251b55847679de6e2c56
Kimberly Howard <kimberlyhoward529@gmail.com>
2023-07-13 23:49:45 -0400

    Added Alpha Markup for Sign in Page
    Installed 3 Font Awesome Dependencies (free brands icons, react font awesome icons, font awesome svg core) that were used for the one click sign in options
    
    Installed Google Font through next/font dependencies to get custom fonts.
    
    Installed Classnames dependency to use multiple classnames for a single html element.
    
    Added a hover component to the sign in button so user knows when they are hovering over button.

--- app/components/loginForm/LoginForm.js
+++ app/components/loginForm/LoginForm.js
@@ -1,6 +1,13 @@
 "use client"
 import style from "./loginForm.module.css";
 import { useState } from "react";
+import { Lato } from "next/font/google";
+import classNames from "classnames";
+
+const lato = Lato({
+    subsets: ["latin"],
+    weight: "400",
+})
 
 export default function LoginForm () {
     const [email, setEmail] = useState('');
@@ -14,27 +21,34 @@ export default function LoginForm () {
             action="submit" 
             className={style.loginFormContainer}
         >
-            <span className={style.loginTitle}>WELCOME!</span>
-            <input 
-                className={style.loginInput} 
-                type="email" 
-                placeholder="Email, Username, or Phone"
-                onChange={e => setEmail(e.target.value)}
-            required />
+            <div className={style.loginTitleContainer}>
+                <span className={classNames(lato.className, style.loginTitle)}>WELCOME!</span>
+            </div>
+            <div className={style.loginInputContainer}>
+                <input 
+                    className={style.loginInput} 
+                    type="email" 
+                    placeholder="Email, Username, or Phone"
+                    onChange={e => setEmail(e.target.value)}
+                required />
             <input 
                 className={style.loginInput} 
                 type="password"
                 placeholder="Password"
                 onChange={e => setPassword(e.target.value)}
             required />
-            <button 
-                className={style.loginBtn}
-                // disabled={login}
-                type="submit"
-                // onClick={() => loginWithEmailAndPswd()}
-            >
-                SIGN IN!
-            </button>
+            </div>
+            <div className={style.loginBtnContainer}>
+                <button 
+                    className={style.loginBtn}
+                    // disabled={login}
+                    type="submit"
+                    // onClick={() => loginWithEmailAndPswd()}
+                >
+                    SIGN IN!
+                </button>
+            </div>
         </form>
     )
 }
+
--- app/pages/login/index.js
+++ app/pages/login/index.js
@@ -1,11 +1,21 @@
 import LoginForm from "@/app/components/loginForm/LoginForm";
 import style from "./login.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faApple, faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 export default function Login() {
+    const oneClickIcons = [faApple, faFacebook, faGoogle]
     return (
         <div className={style.loginContainer}>
             <div>
                 <LoginForm />
+            </div>
+            <div className={style.oneClickIconContainer}>
+                {oneClickIcons.map((icon) => {
+                    return(
+                        <FontAwesomeIcon className={style.oneClickIcon} icon={icon} />
+                    )
+                })}
             </div>
         </div>
     )
