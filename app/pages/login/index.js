086bb75d37a03ac9b3cde032ace0e97159665035
Kimberly Howard <kimberlyhoward529@gmail.com>
2023-07-18 19:50:06 -0400

    Changed button sizes and refactored google fonts for tailwind
    Added google fonts to layout page.
    
    Updated tailwind.config file.
    
    Removed classNames dependency as it was not relative anymore.

--- app/pages/login/index.js
+++ app/pages/login/index.js
@@ -1,12 +1,5 @@
 import LoginForm from "@/app/components/loginForm/LoginForm";
 import SSO from "@/app/components/sso/SSO";
-import classNames from "classnames";
-import { Open_Sans } from "next/font/google";
-import Link from "next/link";
-
-const openSans = Open_Sans({
-    subsets: ["latin"],
-})
 
 export default function Login() {
     return (
@@ -14,10 +7,7 @@ export default function Login() {
             <LoginForm />
             <SSO />
             <p 
-                className={
-                classNames(
-                "text-grey mt-5 font-light", 
-                openSans.className)}
+                className="text-grey mt-5 font-light font-openSans"
             >{`Don't have an account yet?`} <span className="font-bold hover:underline hover:text-blue cursor-pointer">Sign up!</span>
             </p>
         </div>
--- app/pages/signUp/index.js
+++ app/pages/signUp/index.js
@@ -1,12 +1,5 @@
 import SignUpForm from "@/app/components/signUpForm/SignUpForm";
 import SSO from "@/app/components/sso/SSO";
-import classNames from "classnames";
-import { Open_Sans } from "next/font/google";
-
-const openSans = Open_Sans({
-    subsets: ["latin"],
-    // weight: "300"
-})
 
 export default function SignUp () {
     return (
@@ -14,10 +7,7 @@ export default function SignUp () {
             <SignUpForm />
             <SSO />
             <p 
-                className={
-                classNames(
-                "text-grey mt-5 font-light", 
-                openSans.className)}
+                className="text-grey mt-5 font-light font-openSans"
             >Already have an account? <span className="font-bold hover:underline hover:text-blue cursor-pointer">Sign in!</span>
             </p>
         </div>
