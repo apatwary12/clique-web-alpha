acfdcdc86d6ef400c357419f7804c92a95dded6d
Kimberly Howard <kimberlyhoward529@gmail.com>
2023-07-31 14:58:22 -0600

    Changed overlap issue and removed drawings
    Was able to drawings and logo overlap. However, I am not sure how to go about the blurry issue. I don't notice it being blurry on my side. I did try to resize the clique logo to see if that would help and for me it made it worse. Do you have any ideas?

--- app/components/loginGraphic/LoginGraphic.js
+++ app/components/loginGraphic/LoginGraphic.js
@@ -6,18 +6,18 @@ import Ellipse438 from "@/public/images/Ellipse-438.png";
 
 export default function LoginGraphic () {
     return(
-        <div className="">
-            <div className="relative top-20 left-20">
-                <Image src={Ellipse438} width={250}/>
+        <div className="flex h-full w-full">
+            <div className="relative left-10 top-5">
+                <Image src={Ellipse438} width={200}/>
             </div>
-            <div className="absolute top-80 right-60">
-                <Image src={Clique} width={500} />
+            <div className="z-20 justify-self-center mt-80 shrink">
+                <Image src={Clique} width={400}/>
             </div>
-            <div className="absolute right-0 bottom-0">
-                <Image src={Ellipse436} width={350}/>
+            <div className="absolute right-0 bottom-36 z-10">
+                <Image src={Ellipse437} width={250}/>
             </div>
-            <div className="absolute right-0 bottom-36">
-                <Image src={Ellipse437} width={300}/>
+            <div className="absolute bottom-0 right-0">
+                <Image src={Ellipse436} width={300}/>
             </div>
         </div>
     )
