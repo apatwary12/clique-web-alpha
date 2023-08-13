8c05338eea7464dff2ab9f36efe161fa334d0924
Kimberly Howard <kimberlyhoward529@gmail.com>
2023-08-13 12:59:38 -0400

    Added new images for dots

--- app/components/loginGraphic/LoginGraphic.js
+++ app/components/loginGraphic/LoginGraphic.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import Clique from "@/public/images/CliqueLogo_KO-01.png";
 import CliqueShadow from "@/public/images/CliqueLogo-01.svg"
-import Ellipse436 from "@/public/images/Ellipse-436.png";
-import Ellipse437 from "@/public/images/Ellipse-437.png";
-import Ellipse438 from "@/public/images/Ellipse-438.png";
+import EllipseYellow from "@/public/images/Ellipse-Yellow.png";
+import EllipseOrange from "@/public/images/Ellipse-Orange.png";
+import EllipseWhite from "@/public/images/Ellipse-White.png";
 
 export default function LoginGraphic () {
     return(
         <div className="flex h-full w-full">
             <div className="relative left-10 top-5">
-                <Image src={Ellipse438} width={200}/>
+                <Image src={EllipseWhite} width={200}/>
             </div>
             <div className="z-20 justify-self-center mt-80 shrink">
                 <div className="absolute z-10">
@@ -19,11 +19,11 @@ export default function LoginGraphic () {
                     <Image src={CliqueShadow} width={400}/>
                 </div>
             </div>
-            <div className="absolute right-0 bottom-36 z-10">
-                <Image src={Ellipse437} width={250}/>
+            <div className="absolute w-88 h-88 right-0 bottom-72 z-10 overflow-hidden">
+                <Image src={EllipseOrange} width={300}/>
             </div>
             <div className="absolute bottom-0 right-0">
-                <Image src={Ellipse436} width={300}/>
+                <Image src={EllipseYellow} width={400}/>
             </div>
         </div>
     )
