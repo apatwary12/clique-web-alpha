4befcbb1ba5f919e9155430624ed219ddd624074
Kimberly Howard <kimberlyhoward529@gmail.com>
2023-08-17 09:58:39 -0400

    Added alts to images on login graphic

--- app/components/loginGraphic/LoginGraphic.js
+++ app/components/loginGraphic/LoginGraphic.js
@@ -9,21 +9,21 @@ export default function LoginGraphic () {
     return(
         <div className="relative flex h-full w-full">
             <div className="relative left-10 top-5">
-                <Image src={EllipseWhite} width={250}/>
+                <Image src={EllipseWhite} width={250} alt="white elipse" />
             </div>
             <div className="z-20 justify-self-center mt-80 shrink">
                 <div className="absolute z-10">
-                    <Image src={Clique} width={400}/>
+                    <Image src={Clique} width={400} alt="clique logo" />
                 </div>
                 <div className="relative top-3 opacity-40">
-                    <Image src={CliqueShadow} width={400}/>
+                    <Image src={CliqueShadow} width={400} alt="clique logo shadow" />
                 </div>
             </div>
             <div className="absolute w-88 h-88 right-0 bottom-72 z-10 overflow-hidden">
-                <Image src={EllipseOrange} width={300}/>
+                <Image src={EllipseOrange} width={300} alt="orange elipse" />
             </div>
             <div className="absolute bottom-0 right-0">
-                <Image src={EllipseYellow} width={400}/>
+                <Image src={EllipseYellow} width={400} alt="yellow elipse" />
             </div>
         </div>
     )
