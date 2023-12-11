ac80a284cc1a1b4975bbb81b03bc27f546733e44
Ari <ari.patwary@mediavine.com>
2023-12-10 22:11:13 -0500

    initial

--- app/components/signupContainer/SignupContainer.js
+++ app/components/signupContainer/SignupContainer.js
@@ -7,7 +7,7 @@ export default function SignUpContainer () {
     return (
         <div className="flex flex-col items-center h-screen">
             <SignUpForm />
-            <SSO />
+            {/* <SSO /> */}
             <p 
                 className="text-grey mt-5 font-light font-openSans"
             >Already have an account? <Link href="/login" className="font-bold hover:underline hover:text-blue cursor-pointer">Login!</Link>
